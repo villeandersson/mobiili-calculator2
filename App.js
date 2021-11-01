943fc416b4a157198f7d7d5e43431e114f3c831b
Ville Andersson <winstoni@windowslive.com>
2021-11-02 01:06:40 +0200

    laskimen paluu

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList } from 'react-native';
 
 export default function App() {
+  const [eka, setEka] = useState ("");
+  const [toka, setToka] = useState ("");
+  const [vastaus, setVastaus] = useState ("");
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  const button1Pressed = () => {
+    setData([...data, {key: eka + " + " + toka + " = " + (parseInt(eka)+parseInt(toka))}]);
+    setText("");
+    setVastaus("Result: " + (parseInt(eka)+parseInt(toka)))
+  }
+  const button2Pressed = () => {
+    setData([...data, {key: eka + " - " + toka + " = " + (parseInt(eka)-parseInt(toka))}]);
+    setText("");setVastaus("Result: " + (parseInt(eka)-parseInt(toka)))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>{vastaus}</Text>
+        <TextInput style={styles.input}onChangeText={eka=>setEka(eka)}value={eka} keyboardType="numeric"/>
+        <TextInput style={styles.input}onChangeText={toka=>setToka(toka)}value={toka} keyboardType="numeric"/>
+        <View style={styles.buttons}>
+          <Button onPress={button1Pressed} title="+" />
+          <Button onPress={button2Pressed} title="-" />
+        </View>
+        <View style={styles.list}>
+        <Text>History: </Text>
+        <FlatList data ={data}renderItem={({item})=><Text>{item.key}</Text>}/>
+        </View>
+      </View>
   );
 }
 
@@ -16,6 +40,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 150
   },
+  input: {
+    width:200,
+    borderColor:'gray',
+    borderWidth:1
+  },
+  buttons: {
+    width: 60,
+    paddingTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  list: {
+    marginTop: 15
+  }
 });
